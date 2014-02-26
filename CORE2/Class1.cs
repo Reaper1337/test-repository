faf452e6d5f7b4b2db98672eeaded3b97f88d342
@Reaper1337 <karels@students.zcu.cz>
2014-02-26 11:34:41 +0100

    Commit

---
+++ CORE2/Class1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CORE2
+{
+    public class Class1
+    {
+        String test = "System test";
+
+        public String Test
+        {
+            get { return test; }
+            set { test = value; }
+        }
+        String test2 = "System test";
+
+        public String Test2
+        {
+            get { return test2; }
+            set { test2 = value; }
+        }
+
+        public Class1()
+        {
+            int a = 2;
+        }
+
+        public void test4()
+        {
+            int a = 2;
+        }
+
+        public int test3()
+        {
+            return 3;
+        }
+    }
+}
